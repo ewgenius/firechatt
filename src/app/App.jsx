3e78f25507e429021647743b8bc85241574c6bfc
ewgenius <ewgeniux@gmail.com>
2016-01-28 22:30:05 +0300

    login page upd

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,31 +5,12 @@ import ThemeDecorator from 'material-ui/lib/styles/theme-decorator';
 import Card from 'material-ui/lib/card/card';
 import CardActions from 'material-ui/lib/card/card-actions';
 import CardHeader from 'material-ui/lib/card/card-header';
-import CardMedia from 'material-ui/lib/card/card-media';
+import CardText from 'material-ui/lib/card/card-text';
 import CardTitle from 'material-ui/lib/card/card-title';
-import FlatButton from 'material-ui/lib/flat-button';
+import RaisedButton from 'material-ui/lib/raised-button';
 import Shell from './components/Shell/Shell.jsx';
 import api from './lib/firebase.js';
-
-const styles = {
-  login: {
-    position: 'fixed',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    width: '100%',
-    height: '100%'
-  },
-  loginCard: {
-    width: '320px',
-    margin: '64px auto 0',
-    padding: '16px'
-  },
-  loginButton: {
-    margin: '0 auto'
-  }
-}
+import './App.scss';
 
 export default class App extends React.Component {
   constructor() {
@@ -63,11 +44,24 @@ export default class App extends React.Component {
     if (this.state.user)
       return <Shell user={this.state.user}/>
     else
-      return <div className='login' style={styles.login}>
-        <Card style={styles.loginCard}>
-          <CardHeader title={<b>Welcome in Firechatt!</b>}/>
-          <FlatButton style={styles.loginButton} label="LOGIN" onClick={this.login.bind(this)}/>
+      return <div className='login'>
+        <Card className='login-card'>
+          <CardHeader title='Welcome to Firechatt!'/>
+          <CardText>
+            Simple messenger based on firebase, made with react and material-ui.
+            <br/>
+            <br/>
+            Source code: <a href='https://github.com/ewgenius/firechatt' target='_blank'>github.com/ewgenius/firechatt</a>
+          </CardText>
+          <CardActions>
+            <RaisedButton label='login' onClick={this.login.bind(this)}/>
+          </CardActions>
         </Card>
+        <div className='about'>
+          Author: Evgeniy Khramkov
+          <br />
+          <a href='github.com/ewgenius' target='_blank'>https://github.com/ewgenius</a>
+        </div>
       </div>
   }
 }
